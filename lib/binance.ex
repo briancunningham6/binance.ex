90a5a0262968b1d96fe2b4db056ee0530376a2b7
Brian Cunningham <brianc@kseudo.com>
2024-05-24 17:58:01 +0100

    Trying limit_maker - removed time in force

--- lib/binance.ex
+++ lib/binance.ex
@@ -378,7 +378,6 @@ defmodule Binance do
       |> Map.merge(
         unless(is_nil(iceberg_quantity), do: %{icebergQty: iceberg_quantity}, else: %{})
       )
-      |> Map.merge(unless(is_nil(time_in_force), do: %{timeInForce: time_in_force}, else: %{}))
       |> Map.merge(unless(is_nil(price), do: %{price: format_price(price)}, else: %{}))
 
     case HTTPClient.signed_request_binance("/api/v3/order", arguments, :post) do
